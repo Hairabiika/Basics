5b9c62cf5834214caf5b8130410879eba27a0c4e
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 19:10:08 +0200

    Add files via upload

---
+++ CharityCampaign.cs
@@ -0,0 +1,19 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class DanceHall
+    {
+        public static void Main()
+        {
+            int days = int.Parse(Console.ReadLine());
+            int confectioners = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int corrugations = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            double totalMoney = days * confectioners * ((cakes * 45) + (corrugations * 5.80) + (pancakes * 3.20));
+            Console.WriteLine("{0:F2}", totalMoney * 7 / 8);
+        }
+    }
+}
