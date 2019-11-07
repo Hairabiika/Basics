6b7428dea862c20b3b648d497bc8323281a70c00
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 21:16:22 +0200

    Add files via upload

---
+++ BonusScore.cs
@@ -0,0 +1,27 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class SumSeconds
+    {
+        public static void Main()
+        {
+            double number = int.Parse(Console.ReadLine());
+            double bonus = 0;
+            if (number <= 100) { bonus = 5; }
+            else if (number > 100) { 
+                bonus = number / 5;
+                if (number > 1000) 
+                { 
+                    bonus = number / 10; 
+                }
+            }
+
+            if (number % 2 == 0) { bonus++; }
+            if (number % 10 == 5) { bonus += 2; }
+
+            Console.WriteLine(bonus);
+            Console.WriteLine(bonus + number);
+        }
+    }
+}
