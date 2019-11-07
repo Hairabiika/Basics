e30efed7cb8017ca6d35b73db895ac92117be949
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 19:21:22 +0200

    Add files via upload

---
+++ AlcoholMarket.cs
@@ -0,0 +1,23 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class AlcoholMarket
+    {
+        public static void Main()
+        {
+            double wiskeyPrice = double.Parse(Console.ReadLine());
+            double beerQuantity = double.Parse(Console.ReadLine());
+            double wineQuantity = double.Parse(Console.ReadLine());
+            double rakiaQuantity = double.Parse(Console.ReadLine());
+            double wiskeyQunatity = double.Parse(Console.ReadLine());
+
+            double rakiaPrice = wiskeyPrice / 2;
+            double winePrice = 3 * rakiaPrice / 5;
+            double beerPrice = rakiaPrice / 5;
+
+            double totalPrice = wiskeyPrice * wiskeyQunatity + beerPrice * beerQuantity + winePrice * wineQuantity + rakiaPrice * rakiaQuantity;
+            Console.WriteLine("{0:F2}", totalPrice);
+        }
+    }
+}
