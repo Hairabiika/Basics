fd04b5d8ab738fb8e6724397017e55d92f0a3bf2
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 22:10:03 +0200

    Add files via upload

---
+++ MetricConverter.cs
@@ -0,0 +1,58 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class SpeedInfo
+    {
+        public static void Main()
+        {
+            double number = double.Parse(Console.ReadLine());
+            string entryUnit = Console.ReadLine();
+            string wantedUnit = Console.ReadLine();
+
+            switch (entryUnit)
+            {
+                case "m":
+                    switch (wantedUnit)
+                    {
+                        case "cm":
+                            number *= 100;
+                            break;
+
+                        case "mm":
+                            number *= 1000;
+                            break;
+                    }
+                    break;
+
+                case "cm":
+                    switch (wantedUnit)
+                    {
+                        case "m":
+                            number /= 100;
+                            break;
+
+                        case "mm":
+                            number *= 10;
+                            break;
+                    }
+                    break;
+
+                case "mm":
+                    switch (wantedUnit)
+                    {
+                        case "cm":
+                            number /= 10;
+                            break;
+
+                        case "m":
+                            number /= 1000;
+                            break;
+                    }
+                    break;
+            }
+
+            Console.WriteLine("{0:F3}", number);
+        }
+    }
+}
