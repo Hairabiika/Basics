ca560c248b96dd22d29707a5d84f5cec53f00b8b
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 22:14:26 +0200

    Add files via upload

---
+++ TimePlus15Min.cs
@@ -0,0 +1,30 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class TimePlus15Min
+    {
+        public static void Main()
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 15;
+
+            if (minutes >= 60)
+            {
+                minutes -= 60;
+                hours++;
+
+                if (hours == 24)
+                {
+                    hours = 0;
+                }
+            }
+            if (minutes < 10) { Console.WriteLine("{0}:0{1}", hours, minutes);}
+
+            else { Console.WriteLine("{0}:{1}", hours, minutes); }
+            
+        }
+    }
+}
