7d0f93d74cb6cb4690c2b1efb9b52826314a8d91
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-08 00:03:39 +0200

    Add files via upload

---
+++ WorldSwimmingRecord.cs
@@ -0,0 +1,29 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class GodzillaVsKong
+    {
+        public static void Main()
+        {
+            double record = double.Parse(Console.ReadLine());
+            double length = double.Parse(Console.ReadLine());
+            double timeForMeter = double.Parse(Console.ReadLine());
+
+            double waterResistancePer15 = Math.Floor(length / 15);
+            double waterResistance = waterResistancePer15 * 12.5;
+
+            double recordChallenge = timeForMeter * length + waterResistance;
+
+            if (recordChallenge < record)
+            {
+                Console.WriteLine("Yes, he succeeded! The new world record is {0:F2} seconds.", recordChallenge);
+            }
+
+            else
+            {
+                Console.WriteLine("No, he failed! He was {0:F2} seconds slower.", recordChallenge - record);
+            }
+        }
+    }
+}
