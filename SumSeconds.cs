d0f04f3224208a4dbbea01402cb394d135b50794
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 21:04:47 +0200

    Add files via upload

---
+++ SumSeconds.cs
@@ -0,0 +1,21 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class AlcoholMarket
+    {
+        public static void Main()
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secindTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            
+            int totalTime = firstTime + secindTime + thirdTime;
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+            if (seconds < 10) { Console.WriteLine("{0}:0{1}", minutes, seconds); }
+            else { Console.WriteLine("{0}:{1}", minutes, seconds); }
+        }
+    }
+}
