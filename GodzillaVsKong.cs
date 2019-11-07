118ffcbce039601d521988a7ed45943e6bc183f2
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 23:49:04 +0200

    Add files via upload

---
+++ GodzillaVsKong.cs
@@ -0,0 +1,28 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class TimePlus15Min
+    {
+        public static void Main()
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int people = int.Parse(Console.ReadLine());
+            double priceClothes = double.Parse(Console.ReadLine());
+
+            double decor = budget / 10;
+            if (people > 150) { priceClothes = priceClothes * 9 / 10; }
+            if (budget < people * priceClothes + decor) 
+            { 
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine("Wingard needs {0:F2} leva more.", (people * priceClothes + decor) - budget);
+            }
+
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine("Wingard starts filming with {0:F2} leva left.", budget - (people * priceClothes + decor));
+            }
+        }
+    }
+}
