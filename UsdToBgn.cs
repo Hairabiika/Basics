e3eb3f4a752d4e135e8c68dc1e7e6c047f27155a
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 18:17:35 +0200

    Add files via upload

---
+++ UsdToBgn.cs
@@ -0,0 +1,13 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class UsdToBgn
+    {
+        public static void Main()
+        {
+            double dollars = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:F2}", dollars * 1.79549);
+        }
+    }
+}
