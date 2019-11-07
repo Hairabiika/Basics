ae545911e2f1db4ed4c9369fd29b0999a689f1a1
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 18:45:00 +0200

    Add files via upload

---
+++ TailoringWorkshop.cs
@@ -0,0 +1,23 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class RecAreaAndPerim
+    {
+        public static void Main()
+        {
+            int broi = int.Parse(Console.ReadLine());
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            double pokrivkiArea = broi * ((length + 2 * 0.3) * (width + 2 * 0.3));
+            double karetaArea = broi * ((length / 2) * (length / 2));
+
+            double priceInUsd = pokrivkiArea * 7 + karetaArea * 9;
+            double priceInBgn = priceInUsd * 1.85;
+
+            Console.WriteLine("{0:F2} USD", priceInUsd);
+            Console.WriteLine("{0:F2} BGN", priceInBgn);
+        }
+    }
+}
