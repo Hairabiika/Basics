3083fa4537c39c2598b0fa121def079df71eef76
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 19:03:14 +0200

    Add files via upload

---
+++ DanceHall.cs
@@ -0,0 +1,20 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class DanceHall
+    {
+        public static void Main()
+        {
+            double hallLength = double.Parse(Console.ReadLine());
+            double hallWidth = double.Parse(Console.ReadLine());
+            double wardrobeSide = double.Parse(Console.ReadLine());
+
+            double hallArea = hallLength * hallWidth * 100 * 100;
+            double wardrobeArea = wardrobeSide * wardrobeSide * 100 * 100;
+            double benchArea = hallArea / 10;
+            double freeSpace = hallArea - wardrobeArea - benchArea;
+            Console.WriteLine(Math.Floor(freeSpace / (7040)));
+        }
+    }
+}
