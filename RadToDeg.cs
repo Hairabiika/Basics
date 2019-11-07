d3ea9b8e1084e8dc9b8b630dbb6ccb29b197825e
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-07 18:21:49 +0200

    Add files via upload

---
+++ RadToDeg.cs
@@ -0,0 +1,13 @@
+﻿namespace Basic
+{
+    using System;
+
+    public class RadToDeg
+    {
+        public static void Main()
+        {
+            double radians = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:F0}", radians * 180 / Math.PI);
+        }
+    }
+}
