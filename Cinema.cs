2b5967e97c1411d3b222091a05cba8bcf58af289
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-08 11:50:20 +0200

    Add files via upload

---
+++ Cinema.cs
@@ -0,0 +1,28 @@
+﻿namespace Basic
+{
+    using System;
+    public class PointOnRecBorder
+    {
+        public static void Main()
+        {
+            string typeProjection = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            switch (typeProjection)
+            {
+                case "Premiere":
+                    Console.WriteLine("{0:F2} leva", rows * cols * 10);
+                    break;
+
+                case "Normal":
+                    Console.WriteLine("{0:F2} leva", rows * cols * 7.5);
+                    break;
+
+                case "Discount":
+                    Console.WriteLine("{0:F2} leva", rows * cols * 5);
+                    break;
+            }
+        }
+    }
+}
