0570d1234bf86e77240a01c087b4412ff5c7789c
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-08 12:00:16 +0200

    Add files via upload

---
+++ SummerOutfit.cs
@@ -0,0 +1,66 @@
+﻿namespace Basic
+{
+    using System;
+    public class Cinema
+    {
+        public static void Main()
+        {        
+            int degree = int.Parse(Console.ReadLine());
+            string dayTime = Console.ReadLine();
+            
+            if (degree >= 10 && degree <= 18)
+            {
+                switch (dayTime)
+                {
+                    case "Morning":
+                        Console.WriteLine("It's {0} degrees, get your Sweatshirt and Sneakers.", degree);
+                        break;
+
+                    case "Afternoon":
+                        Console.WriteLine("It's {0} degrees, get your Shirt and Moccasins.", degree);
+                        break;
+
+                    case "Evening":
+                        Console.WriteLine("It's {0} degrees, get your Shirt and Moccasins.", degree);
+                        break;
+                }
+            }
+
+            else if (degree > 18 && degree <= 24)
+            {
+                switch (dayTime)
+                {
+                    case "Morning":
+                        Console.WriteLine("It's {0} degrees, get your Shirt and Moccasins.", degree);
+                        break;
+
+                    case "Afternoon":
+                        Console.WriteLine("It's {0} degrees, get your T-Shirt and Sandals.", degree);
+                        break;
+
+                    case "Evening":
+                        Console.WriteLine("It's {0} degrees, get your Shirt and Moccasins.", degree);
+                        break;
+                }
+            }
+
+            else
+            {
+                switch (dayTime)
+                {
+                    case "Morning":
+                        Console.WriteLine("It's {0} degrees, get your T-Shirt and Sandals.", degree);
+                        break;
+
+                    case "Afternoon":
+                        Console.WriteLine("It's {0} degrees, get your Swim Suit and Barefoot.", degree);
+                        break;
+
+                    case "Evening":
+                        Console.WriteLine("It's {0} degrees, get your Shirt and Moccasins.", degree);
+                        break;
+                }
+            }
+        }
+    }
+}
