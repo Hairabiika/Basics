2cad67a7e6a70f97700aae1267eea27acbe425c2
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-09 11:14:23 +0200

    Add files via upload

---
+++ Grades.cs
@@ -0,0 +1,44 @@
+﻿namespace Basic
+{
+    using System;
+    public class Grades
+    {
+        public static void Main()
+        {
+            int badGrades = int.Parse(Console.ReadLine());
+            int count = 0;
+            double problems = 0;
+            string lastExercise = "";
+            double allGrades = 0;
+
+            while (badGrades > 0)
+            {
+                string exercise = Console.ReadLine();
+                if (exercise == "Enough")
+                {
+                    Console.WriteLine("Average score: {0:F2}", allGrades/problems);
+                    Console.WriteLine($"Number of problems: {problems}");
+                    Console.WriteLine($"Last problem: {lastExercise}");
+                    break;
+                }
+
+                int grade = int.Parse(Console.ReadLine());
+                lastExercise = exercise;
+                problems++;
+                allGrades += grade;
+                
+                if (grade <= 4) 
+                { 
+                    badGrades--;
+                    count++;
+                }
+
+                if (badGrades == 0) 
+                { 
+                    Console.WriteLine($"You need a break, {count} poor grades."); 
+                    break; 
+                }
+            }
+        }
+    }
+}
