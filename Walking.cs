e5956bbf592501c990f6efa7e3f69f9cd8636598
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-09 12:40:22 +0200

    Add files via upload

---
+++ Walking.cs
@@ -0,0 +1,42 @@
+﻿namespace Basic
+{
+    using System;
+    public class Walking
+    {
+        public static void Main()
+        {
+            string hello = Console.ReadLine();
+            int count = 0;
+            bool success = false;
+
+            while (true)
+            {
+                if (hello == "Going home")
+                {
+                    hello = Console.ReadLine();
+                    count += int.Parse(hello);
+                    if (count >= 10000)
+                    {
+                        Console.WriteLine("Goal reached! Good job!");
+                        success = true;
+                    }
+
+                    break;
+                }
+                
+                count += int.Parse(hello);
+
+                if (count >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    success = true;
+                    break;
+                }
+
+                hello = Console.ReadLine();
+            }
+
+            if (!success) { Console.WriteLine($"{10000-count} more steps to reach goal."); }
+        }
+    }
+}
