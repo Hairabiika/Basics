fd5792946de7b728148f2d468167e644bcd005a2
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-09 10:47:57 +0200

    Add files via upload

---
+++ Volleyball.cs
@@ -0,0 +1,25 @@
+﻿namespace Basic
+{
+    using System;
+    public class OnTimeForExam
+    {
+        public static void Main()
+        {
+            string year = Console.ReadLine();
+            double holidays = double.Parse(Console.ReadLine());
+            double weekendsOutside = double.Parse(Console.ReadLine());
+
+            double weekends = 48 - weekendsOutside;
+            double weekendsWithoutWork = 3 * weekends / 4;
+            double playingHolidays = 2 * holidays / 3;
+
+            double total = weekendsWithoutWork + playingHolidays + weekendsOutside;
+            if (year == "leap") 
+            {
+                double final = Math.Floor(23 * total / 20);
+                Console.WriteLine(final); 
+            }
+            else { Console.WriteLine(Math.Floor(total)); }
+        }
+    }
+}
