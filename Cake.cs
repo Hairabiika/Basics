f464c2fa938ba463770d003021539e3eece8546b
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-09 14:10:49 +0200

    Add files via upload

---
+++ Cake.cs
@@ -0,0 +1,32 @@
+﻿namespace Basic
+{
+    using System;
+    public class Coins
+    {
+        public static void Main()
+        {
+            int legnth = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            int cake = legnth * width;
+            int pieces = 0;
+
+            while (cake >= 0)
+            {
+                string input = Console.ReadLine();
+                if (input == "STOP")
+                {
+                    Console.WriteLine($"{cake - pieces} pieces are left.");
+                    break;
+                }
+
+                pieces += int.Parse(input);
+                if (pieces > cake)
+                {
+                    Console.WriteLine($"No more cake left! You need {pieces-cake} pieces more.");
+                    break;
+                }
+            }
+        }
+    }
+}
