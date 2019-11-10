a07dc23107d4224095e68d45a4c3fefc8a8d8e38
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-10 18:13:08 +0200

    Add files via upload

---
+++ NumbersEndingIn7.cs
@@ -0,0 +1,17 @@
+﻿namespace Basic
+{
+    using System;
+    public class NumbersEndingIn7
+    {
+        public static void Main()
+        {
+            for (int i = 7; i < 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
