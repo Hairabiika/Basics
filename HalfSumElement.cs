4674915ee1664abb515a103bae33516cac067402
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-10 18:24:33 +0200

    Add files via upload

---
+++ HalfSumElement.cs
@@ -0,0 +1,42 @@
+﻿namespace Basic
+{
+    using System;
+    public class NumbersEndingIn7
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int numbers = int.Parse(Console.ReadLine());
+            int max = numbers;
+            int sum = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+                numbers = int.Parse(Console.ReadLine());
+                if (numbers > max)
+                {
+                    sum += max;
+                    max = numbers;
+                }
+                else
+                {
+                    sum += numbers;
+                }
+            }
+
+            if (max == sum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+
+            else
+            {
+                Console.WriteLine("No");
+                int diff = sum - max;
+                if (diff < 0) { diff *= -1; Console.WriteLine($"Diff = {diff}");}
+                else { Console.WriteLine($"Diff = {diff}"); }
+            }
+        }
+    }
+}
