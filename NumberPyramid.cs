c0c2963173d9c9e8b0a859ec317286c40ff99763
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-11 23:08:56 +0200

    Add files via upload

---
+++ NumberPyramid.cs
@@ -0,0 +1,35 @@
+﻿namespace Basic
+{
+    using System;
+    public class Matrix
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int row = 1;
+            int counter = 0;
+            bool a = false;
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = row; j <= row + counter; j++)
+                {
+                    if (j > number) 
+                    {
+                        a = true;
+                        break;
+                    }
+                    Console.Write(j + " ");
+                }
+
+                if (a) { break; }
+                else
+                {
+                    Console.WriteLine();
+                    row += i;
+                    counter++;
+                }
+            }
+        }
+    }
+}
