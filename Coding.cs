d7085cc14b4f3a5634eb0be20192b35154b27745
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-11 23:31:48 +0200

    Add files via upload

---
+++ Coding.cs
@@ -0,0 +1,33 @@
+﻿namespace Basic
+{
+    using System;
+    public class NumberPyramid
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int numbers = number;
+            int numbersCounter = 0;
+
+            while (numbers != 0)
+            {
+                numbers /= 10;
+                numbersCounter++;
+            }
+
+            numbers = number;
+            for (int i = 0; i < numbersCounter; i++)
+            {
+                for (int j = 0; j < numbers % 10; j++)
+                {
+
+                    Console.Write((char)(numbers % 10 + 33));
+                }
+                if (numbers % 10 == 0) { Console.Write("ZERO"); }
+                number /= 10;
+                numbers = number;
+                Console.WriteLine();
+            }
+        }
+    }
+}
