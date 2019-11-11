c1749dd6dcdc217fe89d55e8bf4a72679ce14673
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-12 00:00:58 +0200

    Add files via upload

---
+++ SumPrimeNonPrime.cs
@@ -0,0 +1,38 @@
+﻿namespace Basic
+{
+    using System;
+    public class EqualSumsLeftRight
+    {
+        public static void Main()
+        {
+            int sumOfPrime = 0;
+            int sumOfNonPrime = 0;
+            bool prime = true;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "stop") { break; }
+                int number = int.Parse(input);
+                if (number < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                else
+                {
+                    for (int i = 2; i <= number / 2; i++)
+                    { 
+                        if (number % i == 0) { prime = false; break; }
+                        else { prime = true; }
+                    }
+                    if (prime) { sumOfPrime += number; }
+                    else { sumOfNonPrime += number; }
+                }
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {sumOfPrime}");
+            Console.WriteLine($"Sum of all non prime numbers is: {sumOfNonPrime}");
+
+        }
+    }
+}
