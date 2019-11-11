2218561e267823f65196889ebf51801d89e66d6c
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-11 22:41:02 +0200

    Add files via upload

---
+++ Matrix.cs
@@ -0,0 +1,33 @@
+﻿namespace Basic
+{
+    using System;
+    public class Matrix
+    {
+        public static void Main()
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            for (int a = firstNumber; a <= secondNumber; a++)
+            {
+                for (int b = firstNumber; b <= secondNumber; b++)
+                {
+                    for (int c = thirdNumber; c <= fourthNumber; c++)
+                    {
+                        for (int d = thirdNumber; d <= fourthNumber; d++)
+                        {
+                            if (a + d == b + c && a != b && c != d)
+                            {
+                                Console.WriteLine($"{a}{b}");
+                                Console.WriteLine($"{c}{d}");
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
