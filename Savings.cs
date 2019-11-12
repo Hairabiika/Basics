a90a26eacee7296e13d84f86a5d4a8444ff6e43f
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-12 21:50:59 +0200

    Add files via upload

---
+++ Savings.cs
@@ -0,0 +1,19 @@
+﻿namespace Basic
+{
+    using System;
+    public class SpecialNumbers
+    {
+        public static void Main()
+        {
+            double income = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double sum = double.Parse(Console.ReadLine());
+
+            double unpredictExpence = 3 * income / 10;
+            double saved = income - unpredictExpence - sum;
+
+            Console.WriteLine($"She can save {(saved / income) * 100:F2}%");
+            Console.WriteLine($"{months * saved:F2}");
+        }
+    }
+}
