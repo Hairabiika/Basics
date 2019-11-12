c077d9ad9f93844bfd809fa390c9493d73cce07f
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-12 20:55:50 +0200

    Add files via upload

---
+++ PasswordGenerator.cs
@@ -0,0 +1,40 @@
+﻿namespace Basic
+{
+    using System;
+    public class Fishing
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            double firstDigit;
+            double secondDigit;
+            char digit;
+
+            for (int i = 1; i < n; i++)
+            {               
+                for (int j = 1; j < n; j++)
+                {                    
+                    for (int k = 1; k <= l; k++)
+                    {                       
+                        for (int a = 1; a <= l; a++)
+                        {
+                            for (int s = 2; s <= n; s++)
+                            {
+                                if (s > j && s > i)
+                                {
+                                    Console.Write(i);
+                                    Console.Write(j);
+                                    Console.Write((char)(k + 96));
+                                    Console.Write((char)(a + 96));
+                                    Console.Write(s + " ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
