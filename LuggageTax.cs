4a6d771031e3a36acf2f7669764f5037b2afe9dc
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-13 11:45:25 +0200

    Add files via upload

---
+++ LuggageTax.cs
@@ -0,0 +1,54 @@
+﻿namespace Basic
+{
+    using System;
+    public class LuggageTax
+    {
+        public static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int depth = int.Parse(Console.ReadLine());
+            bool biletType = bool.Parse(Console.ReadLine());
+
+            if (biletType)
+            {
+                int total = length * height * depth;
+                if (total <= 100)
+                {
+                    Console.WriteLine("Luggage tax: 0.00");
+                }
+
+                if (total > 100 && total < 200)
+                {
+                    Console.WriteLine("Luggage tax: 10.00");
+                }
+
+                if (total > 200 && total <= 300)
+                {
+                    Console.WriteLine("Luggage tax: 20.00");
+                }
+
+            }
+
+            else if (!biletType)
+            {
+                int total = length * height * depth;
+                if (total <= 100)
+                {
+                    Console.WriteLine("Luggage tax: 25.00");
+                }
+
+                if (total > 100 && total < 200)
+                {
+                    Console.WriteLine("Luggage tax: 50.00");
+                }
+
+                if (total > 200 && total <= 300)
+                {
+                    Console.WriteLine("Luggage tax: 100.00");
+                }
+            }
+
+        }
+    }
+}
