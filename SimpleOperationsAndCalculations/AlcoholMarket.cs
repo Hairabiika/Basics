97363c20e8ca12e0651987074e6fae98b0006a01
Hayrabed Agopov <57484867+Hairabiika@users.noreply.github.com>
2019-11-14 20:25:58 +0200

    summary of alcohol prices

--- SimpleOperationsAndCalculations/AlcoholMarket.cs
+++ SimpleOperationsAndCalculations/AlcoholMarket.cs
@@ -15,8 +15,8 @@
             double rakiaPrice = wiskeyPrice / 2;
             double winePrice = 3 * rakiaPrice / 5;
             double beerPrice = rakiaPrice / 5;
-
             double totalPrice = wiskeyPrice * wiskeyQunatity + beerPrice * beerQuantity + winePrice * wineQuantity + rakiaPrice * rakiaQuantity;
+            
             Console.WriteLine("{0:F2}", totalPrice);
         }
     }
